7a500887179423aa4b5d54e0c4468f7d5cfe945e
Viktor Stokstad <141615732+Vikkidub@users.noreply.github.com>
2024-01-18 20:42:58 +0100

    Added monthView and time display

--- view.js
+++ view.js
@@ -1,6 +1,63 @@
-WeekView();
-function WeekView(){
+monthView();
+function monthView(){
+    app.innerHTML = /*HTML*/`
+    <table>
+    <tr>
+    <th>M</th>
+    </tr>
+    <tr>
+    <td>1</td>
+    <td>2</td>
+    <td>3</td>
+    <td>4</td>
+    <td>5</td>
+    <td>6</td>
+    <td>7</td>
+    </tr>
+    <tr>
+    <td>8</td>
+    <td>9</td>
+    <td>10</td>
+    <td>11</td>
+    <td>12</td>
+    <td>13</td>
+    <td>14</td>
+    </tr>
+    <tr>
+    <td>15</td>
+    <td>16</td>
+    <td>17</td>
+    <td>18</td>
+    <td>19</td>
+    <td>20</td>
+    <td>21</td>
+    </tr>
+    <tr>
+    <td>22</td>
+    <td>23</td>
+    <td>24</td>
+    <td>25</td>
+    <td>26</td>
+    <td>27</td>
+    <td>28</td>
+    </tr>
+    <tr>
+    <td>29</td>
+    <td>30</td>
+    <td>31</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    </tr>
+    </table>
+    <div id="timeDiv">a</div>
+    `;
+}
+function weekView(){
 app.innerHTML = /*HTML*/` 
+<button>Back</button>
+<p>
 <table>
 <tr>
 <th>Mandag</th>
@@ -12,41 +69,43 @@ app.innerHTML = /*HTML*/`
 <th>Søndag</th>
 </tr>
 <tr>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
 </tr>
 <tr>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
 </tr>
 <tr>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
 </tr>
 <tr>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
-<td><input></td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
+<td>24</td>
 </tr>
 </table>
+<p>
+<div id="timeDiv">a</div>
 `;
 }
