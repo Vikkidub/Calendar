1a814fcda3dab9b2fb3cd59284ae67e376dc0467
Viktor Stokstad <141615732+Vikkidub@users.noreply.github.com>
2024-01-18 19:48:14 +0100

    Added input fields and improved formatting

---
+++ model.js
@@ -0,0 +1,36 @@
+app = document.getElementById('app')
+
+monday1 = "Frokost"
+monday2 = ""
+monday3 = ""
+monday4 = ""
+
+tuesday1 = ""
+tuesday2 = ""
+tuesday3 = ""
+tuesday4 = ""
+
+wednesday1 = ""
+wednesday2 = ""
+wednesday3 = ""
+wednesday4 = ""
+
+thursday1 = ""
+thursday2 = ""
+thursday3 = ""
+thursday4 = ""
+
+friday1 = ""
+friday2 = ""
+friday3 = ""
+friday4 = ""
+
+saturday1 = ""
+saturday2 = ""
+saturday3 = ""
+saturday4 = ""
+
+sunday1 = ""
+sunday2 = ""
+sunday3 = ""
+sunday4 = ""
